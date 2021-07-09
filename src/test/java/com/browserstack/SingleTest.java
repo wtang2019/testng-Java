a0bd71589c71661f0d3eafa07db1e57e7bfac403
wtang2019 <wtang88@gmx.de>
2021-07-09 23:27:33 +0100

    Finalize Code

--- src/test/java/com/browserstack/SingleTest.java
+++ src/test/java/com/browserstack/SingleTest.java
@@ -6,56 +6,38 @@ import org.openqa.selenium.WebDriver;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import org.openqa.selenium.Dimension;
 
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import java.net.URL;
-
-/*
-//import org.junit.Before;
-//import org.junit.After;
-//import static org.junit.Assert.*;
-//import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.core.IsNot.not;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.Keys;
-import java.util.*;
-*/
+
 
 public class SingleTest extends BrowserStackTestNGTest {
 
     @Test
     public void test() throws Exception {
-        String username = System.getenv("BROWSERSTACK_USERNAME");
-        String accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY");
-        String buildName = System.getenv("BROWSERSTACK_BUILD_NAME");
-        String browserstackLocal = System.getenv("BROWSERSTACK_LOCAL");
-        String browserstackLocalIdentifier = System.getenv("BROWSERSTACK_LOCAL_IDENTIFIER");
+        driver.manage().window().maximize();
 
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("build", buildName); // CI/CD job name using BROWSERSTACK_BUILD_NAME env variable
-        capabilities.setCapability("browserstack.local", browserstackLocal);
-        capabilities.setCapability("browserstack.localIdentifier", browserstackLocalIdentifier);
-        capabilities.setCapability("name", "BStack-[Java] Sample Test"); // test buildName
-        WebDriver driver = new RemoteWebDriver(new URL("https://" + username + ":" + accessKey + "@hub.browserstack.com/wd/hub"), capabilities);
-        driver.get("https://www.google.com/");
-        driver.findElement(By.id("L2AGLb")).click();
+        driver.get("https://www.browserstack.com/");
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals("App & Browser Testing for Teams", driver.findElement(By.tagName("h1")).getText()); //First assertion if text is shown as expected in assertEquals
+        WebElement contactUs = driver.findElement(By.xpath("//*[@id=\"post-26\"]/div[1]/div[1]/div/div/article/div/div/div/div[1]/div/div/div/div/span[2]/a"));
+        boolean isVisible = contactUs.isDisplayed(); //Second asseertion to check if Contact Us Button is visible
+        contactUs.click();
 
-        WebElement element = driver.findElement(By.name("q"));
-        element.sendKeys("BrowserStack");
-        element.submit();
-        Thread.sleep(5000);
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        driver.findElement(By.id("support-name")).sendKeys("Wilmer Tang");
+        driver.findElement(By.id("support-email")).sendKeys("wtang88@gmx.de");
 
-        Assert.assertEquals("BrowserStack - Google Search", driver.getTitle());
+        WebElement type = driver.findElement(By.id("support_query_type_chosen"));
+        js.executeScript("window.scrollBy(0,350)", "");
+        type.click();
+
+        driver.findElement(By.xpath("//*[@id=\"support_query_type_chosen\"]/div/ul/li[1]")).click();
+        driver.findElement(By.id("support-message")).sendKeys("It was an enjoyable exercise. Thanks!");
+        driver.findElement(By.id("sales-company")).sendKeys("MyCompany");
+
     }
 }
