9e8c5324c07202d7547d97a674c1488cfc9bc3ba
wtang2019 <wtang88@gmx.de>
2021-07-09 20:23:53 +0100

    Added Env variables for Jenkins

--- src/test/java/com/browserstack/SingleTest.java
+++ src/test/java/com/browserstack/SingleTest.java
@@ -2,9 +2,14 @@ package com.browserstack;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WebDriver;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import java.net.URL;
 
 /*
 //import org.junit.Before;
@@ -31,6 +36,18 @@ public class SingleTest extends BrowserStackTestNGTest {
 
     @Test
     public void test() throws Exception {
+        String username = System.getenv("BROWSERSTACK_USERNAME");
+        String accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY");
+        String buildName = System.getenv("BROWSERSTACK_BUILD_NAME");
+        String browserstackLocal = System.getenv("BROWSERSTACK_LOCAL");
+        String browserstackLocalIdentifier = System.getenv("BROWSERSTACK_LOCAL_IDENTIFIER");
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("build", buildName); // CI/CD job name using BROWSERSTACK_BUILD_NAME env variable
+        capabilities.setCapability("browserstack.local", browserstackLocal);
+        capabilities.setCapability("browserstack.localIdentifier", browserstackLocalIdentifier);
+        capabilities.setCapability("name", "BStack-[Java] Sample Test"); // test buildName
+        WebDriver driver = new RemoteWebDriver(new URL("https://" + username + ":" + accessKey + "@hub.browserstack.com/wd/hub"), capabilities);
         driver.get("https://www.google.com/");
         driver.findElement(By.id("L2AGLb")).click();
 
